3b2a31f39a2cc81976f09b13eec74e967df18b26
Kevin Bennett <kevbuk@gmail.com>
2016-03-03 16:27:30 +0800

    bump version and change moment package dependency to official version

--- package.js
+++ package.js
@@ -1,9 +1,10 @@
 Package.describe({
-  summary: "Fancy date ranges for Moment.js packaged for Meteor."
+  summary: "Fancy date ranges for Moment.js packaged for Meteor.",
+  version: "2.2.0"
 });
 
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
-  api.use('moment', where);
+  api.use('momentjs:moment', where);
   api.add_files('lib/moment-range/lib/moment-range.js', where);
 });
