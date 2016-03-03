bc6f2c5c30daea7e872005838d61da1dca62392d
Kevin Bennett <kevbuk@gmail.com>
2016-03-03 17:43:31 +0800

    moved moment-range code into repo - no longer working as submodule

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "kevbuk:moment-range",
   summary: "Fancy date ranges for Moment.js packaged for Meteor.",
-  version: "2.2.0",
+  version: "2.2.1",
   git: "https://github.com/kevb/meteor-moment-range"
 });
 
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
   api.use('momentjs:moment@2.11.2', where);
-  api.add_files('lib/moment-range/lib/moment-range.bare.js', where);
+  api.add_files('lib/moment-range.js', where);
 });
