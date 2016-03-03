0c745aeb6d64172b984a3a019bb31741fba9d0cd
Kevin Bennett <kevbuk@gmail.com>
2016-03-03 16:31:17 +0800

    specify moment version (2.11.2)

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   name: "kevbuk:moment-range",
   summary: "Fancy date ranges for Moment.js packaged for Meteor.",
-  version: "2.2.0"
+  version: "2.2.0",
+  git: "https://github.com/kevb/meteor-moment-range"
 });
 
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
-  api.use('momentjs:moment', where);
+  api.use('momentjs:moment@2.11.2', where);
   api.add_files('lib/moment-range/lib/moment-range.js', where);
 });
